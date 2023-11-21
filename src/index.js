2a6347b0e7a909ff2a6298045ec8c3d995d6dc87
LG Carvalho <123175326+Luiz-Gustavo-de-Carvalho@users.noreply.github.com>
2023-11-20 21:16:45 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,30 @@
-let nome = "felipe";
-console.log(nome);
+var Nome ;
+var xp  ;
+
+if(xp <= 1000){
+    console.log(" ferro")
+
+} else if(1001 < xp && xp <= 2000){
+        console.log(" Bronze")
+        
+
+    } else if(2001 < xp && xp <= 5000){
+            console.log(" Prata")
+            
+        } else if(5001 < xp && xp <= 7000){
+                console.log(" Ouro")
+                
+            } else if(7001 < xp && xp <= 8000){
+                    console.log(" Platina")
+                    
+                }else if(8001 < xp && xp <= 9000){
+                        console.log(" Ascendente")
+                        
+                    } else if(9001 < xp && xp <= 10000){
+                            console.log(" Imortal")
+                        
+                    }else if( xp >= 10001){
+                                console.log(" Radiante")
+                                };
+
+                                console.log ("O Herói de nome" + Nome + "está no nível de "+ xp);
